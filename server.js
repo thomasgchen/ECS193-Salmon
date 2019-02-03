adcf4b1fd991179f0a04cc08677e9e52bbdadf3e
Thomas Chen <tchen@Thomas-MacBook-Air.local>
2019-02-02 22:44:02 -0800

    Setup react

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.json());
 app.use(
