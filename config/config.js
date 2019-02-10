039578f67ac95670d4193b53cddbdc6db1919f11
Thomas Chen <tchen@Thomas-MacBook-Air.local>
2019-02-09 23:51:18 -0800

    Update config

--- config/config.js
+++ config/config.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    username: 'tchen',
-    password: null,
-    database: 'ecs193_database_development',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
     dialect: 'postgres'
   },
   test: {
-    username: 'tchen',
-    password: null,
-    database: 'ecs193_database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
     dialect: 'postgres'
   },
   production: {
