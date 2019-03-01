7a8f4bd9837b82ee0fa7386f7fee081231452693
Jacob Bevilacqua <jacobbev@gmail.com>
2019-03-01 15:21:42 -0800

    Add error reporting

--- client/src/components/Entries/index.js
+++ client/src/components/Entries/index.js
@@ -7,7 +7,9 @@ import EntryItem from './EntryItem';
 import Filters from './Filters';
 import _ from 'lodash';
 import Grid from '@material-ui/core/Grid';
-import { CenteredProgress } from '../Progress';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import { RECORDS_PER_PAGE } from '../../config/constants';
@@ -17,7 +19,11 @@ const dateFields = ['date'];
 const ignoredFields = ['Location.name'];
 const aliasedFields = { LocationId: 'location' };
 
-const styles = {
+const styles = theme => ({
+  outerRoot: {
+    height: '100%',
+    width: '100%'
+  },
   root: {
     maxHeight: '100%',
     width: '100%',
@@ -34,15 +40,19 @@ const styles = {
     width: '100%',
     overflowX: 'hidden',
     overflowY: 'scroll'
+  },
+  close: {
+    padding: theme.spacing.unit / 2
   }
-};
+});
 
 export class Entries extends Component {
   constructor(props) {
     super(props);
     this.state = {
       filters: {},
-      page: 0
+      page: 0,
+      snackbarOpen: false
     };
   }
 
@@ -51,6 +61,28 @@ export class Entries extends Component {
     this.props.fetchLocations();
   }
 
+  componentDidUpdate(prevProps) {
+    const newErrorMessage = this.constructErrorMessage(
+      this.props.cases.error,
+      this.props.locations.error
+    );
+    const error = newErrorMessage;
+    const prevError = this.constructErrorMessage(prevProps.cases.error, prevProps.locations.error);
+
+    if (prevError !== error && newErrorMessage) {
+      this.setState({ snackbarOpen: true });
+    }
+  }
+
+  constructErrorMessage = (casesError, locationsError) => {
+    let errorString = '';
+    if (casesError && casesError !== null && casesError !== undefined)
+      errorString += String(casesError);
+    if (locationsError && locationsError !== null && locationsError !== undefined)
+      errorString += String(locationsError);
+
+    return errorString;
+  };
   fetchCasesBasedOnState() {
     this.props.fetchCases(this.state.page, this.state.filters);
   }
@@ -73,6 +105,14 @@ export class Entries extends Component {
       }
     });
     return row;
+  };
+
+  handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ snackbarOpen: false });
   };
 
   handleUpdate = (id, data) => {
@@ -112,58 +152,96 @@ export class Entries extends Component {
   };
 
   render() {
-    const { error, loading: casesLoading, items, noMoreToLoad } = this.props.cases;
-    const { loading: locationsLoading, items: locations, lookup } = this.props.locations;
+    const { error: casesError, loading: casesLoading, items, noMoreToLoad } = this.props.cases;
+    const {
+      error: locationsError,
+      loading: locationsLoading,
+      items: locations,
+      lookup
+    } = this.props.locations;
     const loading = casesLoading || locationsLoading;
+    const error = this.constructErrorMessage(casesError, locationsError);
     const { filters, page } = this.state;
 
     return (
-      <Grid
-        container
-        spacing={32}
-        direction="column"
-        alignItems="center"
-        justify="flex-start"
-        className={this.props.classes.root}
-      >
-        <Grid xs={2} item className={this.props.classes.filtersSection}>
-          <Filters
-            locations={locations}
-            handleFilterChange={this.handleFilterChange}
-            values={filters}
-          />
+      <div className={this.props.classes.outerRoot}>
+        <Grid
+          container
+          spacing={32}
+          direction="column"
+          alignItems="center"
+          justify="flex-start"
+          className={this.props.classes.root}
+        >
+          <Grid xs={2} item className={this.props.classes.filtersSection}>
+            <Filters
+              locations={locations}
+              handleFilterChange={this.handleFilterChange}
+              values={filters}
+            />
+          </Grid>
+          <Grid xs={10} item className={this.props.classes.scrollableSection}>
+            {items.slice(0, RECORDS_PER_PAGE * (page + 1)).map(item => {
+              return (
+                <EntryItem
+                  id={item.id}
+                  key={item.id + item.caseNum}
+                  fields={this.structuredFields(item)}
+                  handleUpdate={this.handleUpdate}
+                  handleDelete={this.handleDelete}
+                  extraData={{ locations: locations, lookupTable: lookup }}
+                />
+              );
+            })}
+            <div
+              style={{ textAlign: 'center', marginBottom: '10px' }}
+              onClick={this.handleLoadMore}
+            >
+              {loading && (
+                <div style={{ marginBottom: '10px' }}>
+                  <CircularProgress color="secondary" />
+                </div>
+              )}
+              {noMoreToLoad ? (
+                <p style={{ color: '#bdbdbd' }}>
+                  <i>all records loaded</i>
+                </p>
+              ) : (
+                <Button variant="contained" disabled={loading}>
+                  Load More
+                </Button>
+              )}
+            </div>
+          </Grid>
         </Grid>
-        <Grid xs={10} item className={this.props.classes.scrollableSection}>
-          {items.slice(0, RECORDS_PER_PAGE * (page + 1)).map(item => {
-            return (
-              <EntryItem
-                id={item.id}
-                key={item.id + item.caseNum}
-                fields={this.structuredFields(item)}
-                handleUpdate={this.handleUpdate}
-                handleDelete={this.handleDelete}
-                extraData={{ locations: locations, lookupTable: lookup }}
-              />
-            );
-          })}
-          <div style={{ textAlign: 'center', marginBottom: '10px' }} onClick={this.handleLoadMore}>
-            {loading && (
-              <div style={{ marginBottom: '10px' }}>
-                <CircularProgress color="secondary" />
-              </div>
-            )}
-            {noMoreToLoad ? (
-              <p style={{ color: '#bdbdbd' }}>
-                <i>all records loaded</i>
-              </p>
-            ) : (
-              <Button variant="contained" disabled={loading}>
-                Load More
-              </Button>
-            )}
-          </div>
-        </Grid>
-      </Grid>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={this.state.snackbarOpen}
+          autoHideDuration={5000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          message={<span id="message-id">{error}</span>}
+          action={[
+            // <Button key="undo" color="secondary" size="small" onClick={this.handleClose}>
+            //   UNDO
+            // </Button>,
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={this.props.classes.close}
+              onClick={this.handleSnackbarClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
+      </div>
     );
   }
 }
