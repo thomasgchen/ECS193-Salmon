3a1e67106e9ea30d278672db47ca2e3e109072d6
Jacob Bevilacqua <jacobbev@gmail.com>
2019-03-05 17:43:29 -0800

    Changes to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,17 @@
 import { applyMiddleware, createStore, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 // import loggerMiddleware from './middleware/logger';
 
-const middleware = applyMiddleware(thunkMiddleware);
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+    // other store enhancers if any
+  )
+);
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(rootReducer, composeEnhancers(middleware));
+export default store;
